7889f2440a155d144867403694b644ed9ecd199c
Charlie Phillips <hi.charliephillips@gmail.com>
2020-02-10 17:28:27 -0600

    added textasset class

--- Singleton_CharliePhillips/Assets/Scripts/FileSystem.cs
+++ Singleton_CharliePhillips/Assets/Scripts/FileSystem.cs
@@ -1,18 +1,79 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
-public class FileSystem : MonoBehaviour
+public class FileSystem
 {
-    // Start is called before the first frame update
-    void Start()
+    protected static FileSystem _filesystem;
+
+    private string path; //Private instance data mamber
+    public string Path
     {
-        
+        get { return path; }
+        set { this.path = value; }
     }
 
-    // Update is called once per frame
-    void Update()
+    public static FileSystem Instance
     {
-        
+        get
+        {
+            if (_filesystem == null)
+                _filesystem = new FileSystem();
+            return _filesystem;
+        }
+    }
+
+    protected FileSystem()
+    {
+
+    }
+
+    public virtual void CreateFolder(string name)
+    {
+        //TODO
+    }
+
+    public virtual void DeleteFolder(string name)
+    {
+        //TODO
+    }
+
+    public virtual void DeleteFile(string fileName)
+    {
+        bool fileExists = System.IO.File.Exists(System.IO.Path.Combine(Path, fileName));
+        if (fileExists)
+        {
+            FileInfo fi = new FileInfo(System.IO.Path.Combine(Path, fileName));
+            fi.Delete();
+        }
+    }
+
+    public virtual void CreateTextFile(string fileName, string fileContents)
+    {
+
+
+        using (StreamWriter writer = new StreamWriter(System.IO.Path.Combine(Path, fileName), true))
+        {
+            writer.Write(fileContents);
+        };
+
+    }
+
+    public virtual System.IO.FileStream GetOpenORCreateFileStream(string Name)
+    {
+        return new System.IO.FileStream(System.IO.Path.Combine(Path, Name), System.IO.FileMode.OpenOrCreate);
+    }
+
+    public string ReadTextFile(string Name)
+    {
+        string fileContents;
+        FileStream fileStream = GetOpenORCreateFileStream(Name);
+        using (StreamReader reader = new StreamReader(fileStream))
+        {
+            fileContents = reader.ReadToEnd();
+        }
+        return fileContents;
+
     }
 }
---
+++ Singleton_CharliePhillips/Assets/Scripts/TextAsset.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+public class TextAsset : FileSystem
+{
+    private string _text;
+
+    protected TextAsset(string text)
+    {
+        _text = text;
+    }
+
+    public override void CreateFolder(string name)
+    {
+        //TODO
+    }
+
+    public override void DeleteFolder(string name)
+    {
+        //TODO
+    }
+
+    public override void DeleteFile(string fileName)
+    {
+      
+    }
+
+    public override void CreateTextFile(string fileName, string fileContents)
+    {
+        
+    }
+
+    public override System.IO.FileStream GetOpenORCreateFileStream(string Name)
+    {
+        return new System.IO.FileStream(System.IO.Path.Combine(Path, Name), System.IO.FileMode.OpenOrCreate);
+    }
+}
